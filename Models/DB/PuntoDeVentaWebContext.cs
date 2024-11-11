e33430920eaa1e90cba434edd7d6571f66bcb928
SegoB54 <166057357+SegoBM@users.noreply.github.com>
2024-11-11 17:51:08 -0600

    Cambios remotos

--- Models/DB/PuntoDeVentaWebContext.cs
+++ Models/DB/PuntoDeVentaWebContext.cs
@@ -28,7 +28,7 @@ public partial class PuntoDeVentaWebContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost; Database=PuntoDeVentaWeb; User Id=sa; Password=Expecto89$; Trusted_Connection=False; Encrypt=False;");
+        => optionsBuilder.UseSqlServer("Server=PuntoDeVentaWeb.mssql.somee.com;Database=PuntoDeVentaWeb; User Id=Sego540_SQLLogin_1; Password=pzypvib92m; Trusted_Connection=False; Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
